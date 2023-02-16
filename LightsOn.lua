9861bd88c4ca8adf46f21e3069f0c1cc1ce4b015
Mathew Byrne <mathewbyrne@gmail.com>
2023-02-16 23:49:39 +1100

    Add code to handle relayout of frames

--- LightsOn.lua
+++ LightsOn.lua
@@ -19,31 +19,21 @@ function LightsOn.new()
     local self = {}
     setmetatable(self, LightsOn)
 
-    local frame = CreateFrame("Frame", nil, UIParent)
-    frame:SetSize((2 * ICON_SIZE) + ICON_PADDING, ICON_SIZE)
-    frame:SetPoint("BOTTOMRIGHT", UIParent, "CENTER", -238, -170)
+    self.frame = CreateFrame("Frame", nil, UIParent)
 
-    local iconBeacon = LightsOn_Icon.new(SPELL_BEACON_IDS[1], frame, ICON_SIZE)
-    iconBeacon.frame:SetPoint("RIGHT", frame, "RIGHT")
+    self.iconBeacon = LightsOn_Icon.new(SPELL_BEACON_IDS[1], self.frame)
+    self.iconShield = LightsOn_Icon.new(SPELL_SHIELD_IDS[1], self.frame)
+    self.iconGrace = LightsOn_Icon.new(SPELL_GRACE_IDS[1], self.frame)
+    self.iconJoP = LightsOn_Icon.new(SPELL_JOP_IDS[1], self.frame)
+    self.iconJoL = LightsOn_Icon.new(SPELL_JOL_IDS[1], self.frame)
 
-    local iconShield = LightsOn_Icon.new(SPELL_SHIELD_IDS[1], frame, ICON_SIZE)
-    iconShield.frame:SetPoint("RIGHT", iconBeacon.frame, "LEFT", -ICON_PADDING, 0)
-
-    local iconGrace = LightsOn_Icon.new(SPELL_GRACE_IDS[1], frame, ICON_SIZE)
-    iconGrace.frame:SetPoint("RIGHT", iconShield.frame, "LEFT", -ICON_PADDING, 0)
-
-    local iconJoP = LightsOn_Icon.new(SPELL_JOP_IDS[1], frame, ICON_SIZE)
-    iconJoP.frame:SetPoint("RIGHT", iconGrace.frame, "LEFT", -ICON_PADDING, 0)
-
-    local iconJoL = LightsOn_Icon.new(SPELL_JOL_IDS[1], frame, ICON_SIZE)
-    iconJoL.frame:SetPoint("RIGHT", iconJoP.frame, "LEFT", -ICON_PADDING, 0)
-
-    self.frame = frame
-    self.iconBeacon = iconBeacon
-    self.iconShield = iconShield
-    self.iconGrace = iconGrace
-    self.iconJoL = iconJoL
-    self.iconJoP = iconJoP
+    self:layout({
+        iconSize = ICON_SIZE,
+        iconPadding = ICON_PADDING,
+        x = -238,
+        y = -170,
+        position = "BOTTOMRIGHT",
+    })
 
     self.jolCache = {}
 
@@ -97,8 +87,8 @@ function LightsOn.new()
         self.iconJoL:setProgress(progress)
     end)
 
-    frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-    frame:SetScript("OnEvent", function(_, event)
+    self.frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+    self.frame:SetScript("OnEvent", function(_, event)
         if event == "COMBAT_LOG_EVENT_UNFILTERED" then
             _, subEvent, _, sourceGUID, _, _, _, destGUID, _, _, _, spellID = CombatLogGetCurrentEventInfo()
             if (subEvent == "SPELL_AURA_APPLIED" or subEvent == "SPELL_AURA_REFRESH") and spellID == SPELL_JOL_IDS[1] and sourceGUID == UnitGUID("player") then
@@ -116,4 +106,23 @@ function LightsOn.new()
     return self
 end
 
-LightsOn.new()
+function LightsOn:layout(config)
+    self.frame:SetSize((2 * config.iconSize) + config.iconPadding, config.iconSize)
+
+    self.iconBeacon:setSize(config.iconSize)
+    self.iconShield:setSize(config.iconSize)
+    self.iconGrace:setSize(config.iconSize)
+    self.iconJoL:setSize(config.iconSize)
+    self.iconJoP:setSize(config.iconSize)
+
+    self.frame:ClearAllPoints()
+    self.frame:SetPoint(config.position, UIParent, "CENTER", config.x, config.y)
+    
+    self.iconBeacon.frame:SetPoint("RIGHT", self.frame, "RIGHT")
+    self.iconShield.frame:SetPoint("RIGHT", self.iconBeacon.frame, "LEFT", -config.iconPadding, 0)
+    self.iconGrace.frame:SetPoint("RIGHT", self.iconShield.frame, "LEFT", -config.iconPadding, 0)
+    self.iconJoP.frame:SetPoint("RIGHT", self.iconGrace.frame, "LEFT", -config.iconPadding, 0)
+    self.iconJoL.frame:SetPoint("RIGHT", self.iconJoP.frame, "LEFT", -config.iconPadding, 0)
+end
+
+addon.lightsOn = LightsOn.new()
--- LightsOn_Icon.lua
+++ LightsOn_Icon.lua
@@ -4,21 +4,20 @@ local BORDER_WIDTH = 1
 LightsOn_Icon = {};
 LightsOn_Icon.__index = LightsOn_Icon
 
-function LightsOn_Icon.new(spellName, parent, size)
+local ICON_FONT = "Interface\\AddOns\\ElvUI\\Core\\Media\\Fonts\\Expressway.ttf"
+
+function LightsOn_Icon.new(spellName, parent)
     local self = {}
     setmetatable(self, LightsOn_Icon)
 
     local frame = CreateFrame("Frame", nil, parent)
-    frame:SetSize(size, size)
     frame:SetPoint("CENTER")
 
     local texture = frame:CreateTexture(nil, "BACKGROUND")
     texture:SetPoint("CENTER")
-    texture:SetSize(size, size)
     texture:SetTexture(select(3, GetSpellInfo(spellName)))
 
     local progress = frame:CreateTexture(nil, "OVERLAY")
-    progress:SetSize(size,0)
     progress:SetPoint("BOTTOM")
     progress:SetColorTexture(1.0,0.0,0.0,0.5)
     progress:Hide()
@@ -26,9 +25,7 @@ function LightsOn_Icon.new(spellName, parent, size)
     local timer = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal");
     timer:SetPoint("CENTER")
     timer:SetTextColor(1.0, 1.0, 1.0)
-    timer:SetFont("Interface\\AddOns\\ElvUI\\Core\\Media\\Fonts\\Expressway.ttf", math.ceil(size / 2), "OUTLINE")
 
-    self.size = size
     self.frame = frame
     self.texture = texture
     self.timer = timer
@@ -37,6 +34,14 @@ function LightsOn_Icon.new(spellName, parent, size)
     self:disable()
 
     return self
+end
+
+function LightsOn_Icon:setSize(size)
+    self.size = size
+    self.frame:SetSize(size, size)
+    self.texture:SetSize(size, size)
+    self.progress:SetSize(size, 0)
+    self.timer:SetFont(ICON_FONT, math.ceil(size / 2), "OUTLINE")
 end
 
 function LightsOn_Icon:disable()
